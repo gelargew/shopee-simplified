364762e1243cb81f26350c20434d5b4f9593d3d1
unknown <gelargew@gmail.com>
2021-11-03 23:13:37 +0700

    layout & first sections

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,38 +10,40 @@ export default function Layout({...props}) {
     return (
         <>
             <header>
-                <div id='header-contact'>
-                    <a href='#'>Seller Center</a>|
-                    <a href='#'>Download</a>|
-                    <span>
-                        Ikuti kami di 
-                    </span>
-                    <a href='#'>f</a>
-                    <a href='#'>i</a>
-                </div>
-                
-                <div id='header-user-profile'>
-                    <a href='#'>Notifikasi</a>
-                    <a href='#'>Bantuan</a>
-                    <a href="#">Daftar</a>|
-                    <a href='#'>Login</a>
-                </div>
+                <div className='header-container'>
+                    <div id='header-contact'>
+                        <a href='#'>Seller Center</a>|
+                        <a href='#'>Download</a>|
+                        <span>
+                            Ikuti kami di 
+                        </span>
+                        <a href='#'>f</a>
+                        <a href='#'>i</a>
+                    </div>
+                    
+                    <div id='header-user-profile'>
+                        <a href='#'>Notifikasi</a>
+                        <a href='#'>Bantuan</a>
+                        <a href="#">Daftar</a>|
+                        <a href='#'>Login</a>
+                    </div>
 
-                <a href='#' id='header-logo'>
-                    <Logo />
-                </a>
+                    <a href='#' id='header-logo'>
+                        <Logo />
+                    </a>
 
-                <input id='search-bar' placeholder='search' />
+                    <input id='search-bar' placeholder='search' />
 
-                <div id='header-latest-search'>
-                    <a href='#'>Wacom One</a>
-                    <a href='#'>RTX 3090</a>
-                    <a href='#'>M1</a>
+                    <div id='header-latest-search'>
+                        <a href='#'>Wacom One</a>
+                        <a href='#'>RTX 3090</a>
+                        <a href='#'>M1</a>
+                    </div>
+                    
+                    <a href='#' id='header-cart'>
+                        <ShoppingCart />
+                    </a>
                 </div>
-                
-                <a href='#' id='header-cart'>
-                    <ShoppingCart />
-                </a>
             </header>
             {props.children}
         </>
--- src/components/News.tsx
+++ src/components/News.tsx
@@ -16,6 +16,32 @@ export default function News({...props}) {
             </div>
             <div id='promo-1'>PROMO 1</div>
             <div id='promo-2'>PROMO 2</div>
+            <div id='promo-links'>
+                <a href='#'>
+                    <img src='https://cf.shopee.co.id/file/d7385c2d6fb6d57c647a09fad1114e14_xhdpi' />
+                    Shopee Pilih Lokal
+                </a>
+                <a href='#'>
+                    <img src='https://cf.shopee.co.id/file/fe02a504f64b146f90af16947ae65d08_xhdpi' />
+                    Elektronik 100% ori
+                </a>
+                <a href='#'>
+                    <img src='https://cf.shopee.co.id/file/5164f9b1e9319def7e609101eb3c9349_xhdpi' />
+                    inFashion
+                </a>
+                <a href='#'>
+                    <img src='https://cf.shopee.co.id/file/4447bdcba3c51c693feef4f22ba05c49_xhdpi' />
+                    Shopee Supermarket
+                </a>
+                <a href='#'>
+                    <img src='https://cf.shopee.co.id/file/4e08bce7d8d6ab488b82ab59d03cb793_xhdpi' />
+                    Murah Lebay
+                </a>
+                <a href='#'>
+                    <img src='https://cf.shopee.co.id/file/60434906cac999e73fdcc78c8f484467_xhdpi' />
+                    Semua Promo
+                </a>
+            </div>
         </section>
     )
 }
