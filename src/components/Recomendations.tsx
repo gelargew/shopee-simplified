76255f35ab9c405ea80ec92850d951897b65f224
unknown <gelargew@gmail.com>
2021-11-08 13:12:09 +0700

    recomendation section

--- src/components/Recomendations.tsx
+++ src/components/Recomendations.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const PRODUCTS = [
-    {
+const PRODUCT =  {
         id: 43312,
         name: 'hallowen props',
         imageURL: '/images/sale1.jpg',
         price: 99999.99,
         sold_count: 1000,
         seller: {
-            status: 'star',
+            status: 'Star',
             name: 'Admin'
         }
     }
-]
+
+
+const ARR = new Array(100).fill(0)
 
 export default function Recomendations() {
 
@@ -20,8 +21,8 @@ export default function Recomendations() {
         <section id='section-recomendations'>
             <h4>REKOMENDASI</h4>
             <div className='products'>
-                {PRODUCTS.map(PRODUCT => 
-                    <div key={PRODUCT.id} className='product'>
+                {ARR.map((num, idx) => 
+                    <div key={idx} className='product'>
                         {PRODUCT.seller.status && <div className='seller-status'>{PRODUCT.seller.status}</div>}
                         <img src={PRODUCT.imageURL}/>
                         <p>{PRODUCT.name}</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import ImageSlider from '../components/commons/ImageSlider'
 import Layout from '../components/Layout'
 import Mall from '../components/Mall'
 import News from '../components/News'
+import Recomendations from '../components/Recomendations'
 import '../styles/index.css'
 
 
@@ -14,6 +15,7 @@ export default function Index() {
         <News />
         <Categories />
         <Mall />
+        <Recomendations />
       </main>
     </Layout>
   )
