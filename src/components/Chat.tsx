9d23be748cf43fe9afbe25407d8d6b505588362c
unknown <gelargew@gmail.com>
2021-11-09 14:51:29 +0700

    add chat

---
+++ src/components/Chat.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const CHAT_CONTACTS = [
+    {
+        name: 'offline chat'
+    }
+]
+
+export default function Chat() {
+    const [isActive, setIsActive] = useState(false)
+
+    return (
+        <div className={isActive ? 'chat-container active' : 'chat-container'}>
+            <div className='chat-toggle' onClick={() => setIsActive(!isActive)} >
+                Chat
+            </div>
+            {isActive &&   
+            <>         
+                <div className='chat-box'>
+                    
+                </div>
+                <textarea className='chat-input' placeholder='Tulis pesan' />
+                <div className='chat-contacts'>
+                    {CHAT_CONTACTS.map(contact => 
+                    <div className='chat-contact' key={contact.name}>
+                        {contact.name}    
+                    </div>)}
+                </div>
+            </>}
+        </div>
+    )
+}
