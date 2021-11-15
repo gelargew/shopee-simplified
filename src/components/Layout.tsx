01346841769f43a21d6a1407a57aba0e1b191b42
unknown <gelargew@gmail.com>
2021-11-16 01:39:06 +0700

    add watermark and favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -38,6 +38,8 @@ export default function Layout({...props}) {
                     veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
                     commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam 
                     nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+
+                    <a href='https://gelar.vercel.app/' target='_blank'><small>@gelar rustiawan</small></a>
             </footer>           
         </>
     )
